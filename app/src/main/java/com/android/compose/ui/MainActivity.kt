03407f22229f86c43f0e333654ab6947acda24c5
Simao Felgueiras <ssrfelgueiras@gmail.com>
2021-05-20 17:26:02 +0100

    Creating DogsList layout

--- app/src/main/java/com/android/compose/ui/MainActivity.kt
+++ app/src/main/java/com/android/compose/ui/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.android.compose.data.remote.entities.Dog
+import com.android.compose.ui.dogs.DogsScreen
 import com.android.compose.ui.dogs.DogsViewModel
 import com.android.compose.ui.navigation.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,29 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     private val viewModel: DogsViewModel by viewModels()
 
+    val tmpDogList = listOf(Dog("Teste"),Dog("Teste"),Dog("Teste"),Dog("Teste"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MaterialTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    DogsScreen(Modifier,tmpDogList)
                 }
             }
 
         }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/android/compose/ui/dogs/DogsComposable.kt
+++ app/src/main/java/com/android/compose/ui/dogs/DogsComposable.kt
@@ -1,4 +1,44 @@
 package com.android.compose.ui.dogs
 
-class DogsComposable {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.android.compose.data.remote.entities.Dog
+
+
+@Composable
+fun DogsScreen(modifier: Modifier = Modifier,
+               dogsList: List<Dog>){
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(dogsList) { dogs ->
+            DogCard(
+                modifier = Modifier.fillMaxWidth(),
+                dogPicture = dogs.status ?:""
+            )
+        }
+    }
+}
+
+@Composable
+fun DogCard( modifier: Modifier = Modifier,
+             dogPicture:String){
+
+    Card(modifier = modifier, elevation = 8.dp) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            //TODO to be replaced with an image
+            Text(text = dogPicture)
+        }
+    }
+
 }
